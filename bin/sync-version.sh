6b12885fc63b2c30cf5a452eb3bbf707e9ba10b7
UIT-23730009 <149002453+UIT-23730009@users.noreply.github.com>
2025-10-28 04:30:14 +0000

    feat: add changesets, mod sync-version

--- bin/sync-version.sh
+++ bin/sync-version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-VERSION=$(cat ../VERSION)
 cd ..
-
-#sycn version with /log_output
+VERSION=$(cat ./VERSION)
+echo "Syncing version to $VERSION in all submodules..."
+#sync version with /log_output
 jq --arg ver "$VERSION" '.version = $ver' ./log_output/package.json > tmp.$$.json && mv tmp.$$.json ./log_output/package.json
-git add package.json
+git add ./log_output/package.json
 git commit -m "chore: sync version to $VERSION in package.json"
 git tag "v$VERSION"
 echo "Synced version $VERSION across all submodules."
