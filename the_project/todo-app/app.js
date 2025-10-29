7315a4a2e1f1d9330f879f3c30ab13870be8e810
UIT-23730009 <149002453+UIT-23730009@users.noreply.github.com>
2025-10-29 04:46:37 +0000

    refactor: todo app refactored and add unit test

---
+++ the_project/todo-app/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+
+// Global middleware
+app.use(express.json());
+
+// Register routes
+app.use('/', routes);
+
+// Global error handler (optional, good practice)
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+
+export default app;
